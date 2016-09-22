89b4fc5db08a8dee1c24227a07f2441c730f1b41
David Hernaiz <ladecorreo@gmail.com>
2016-09-22 09:25:11 +0200

    hola bitches

--- app/src/main/java/com/asovel/bibicleta/Interface_Usuario/CatalogoBicicletas/BicicletaActivity.java
+++ app/src/main/java/com/asovel/bibicleta/Interface_Usuario/CatalogoBicicletas/BicicletaActivity.java
@@ -16,7 +16,7 @@ import com.asovel.bibicleta.R;
 public class BicicletaActivity extends AppCompatActivity {
     public RecyclerView recyclerView;
     public LinearLayoutManager linearLayout;
-
+    //Hola ke ase loko?
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
